3d8db4a64dcff10b7c44f8f70d844a42d9ccd886
silverwind <me@silverwind.io>
2015-08-07 23:08:29 +0200

    Updates, reindent

--- cidr-range.js
+++ cidr-range.js
@@ -3,27 +3,27 @@
 var ip = require("ip");
 
 module.exports = function cidrRange(cidr, opts) {
-    if (typeof cidr !== "string") {
-        throw new Error("Expected a string");
-    }
+  if (typeof cidr !== "string") {
+    throw new Error("Expected a string");
+  }
 
-    if (!opts) {
-        opts = {};
-    }
+  if (!opts) {
+    opts = {};
+  }
 
-    var range = ip.cidrSubnet(cidr);
-    var start = ip.toLong(range.networkAddress);
-    var end   = ip.toLong(range.broadcastAddress);
-    var out   = [];
+  var range = ip.cidrSubnet(cidr);
+  var start = ip.toLong(range.networkAddress);
+  var end   = ip.toLong(range.broadcastAddress);
+  var out   = [];
 
-    for (var i = start; i <= end; i++) {
-        out.push(ip.fromLong(i));
-    }
+  for (var i = start; i <= end; i++) {
+    out.push(ip.fromLong(i));
+  }
 
-    if (opts.onlyHosts && range.subnetMaskLength <= 30) {
-        out.splice(0, 1);
-        out.splice(out.length - 1, 1);
-    }
+  if (opts.onlyHosts && range.subnetMaskLength <= 30) {
+    out.splice(0, 1);
+    out.splice(out.length - 1, 1);
+  }
 
-    return out;
+  return out;
 };
--- test.js
+++ test.js
@@ -5,20 +5,18 @@ var assert = require("assert");
 var cr = require("./");
 
 it("should return correct amount of addresses", function () {
-    assert(cr("1.2.3.4/30").length === 4);
-    assert(cr("1.2.3.4/24").length === 256);
-    assert(cr("1.2.3.4/20").length === 4096);
-    assert(cr("255.255.255.255/32").length === 1);
-    assert(cr("1.2.3.4/30", {onlyHosts: true}).length === 2);
-    assert(cr("1.2.3.4/29", {onlyHosts: true}).length === 6);
-
+  assert(cr("1.2.3.4/30").length === 4);
+  assert(cr("1.2.3.4/24").length === 256);
+  assert(cr("1.2.3.4/20").length === 4096);
+  assert(cr("255.255.255.255/32").length === 1);
+  assert(cr("1.2.3.4/30", {onlyHosts: true}).length === 2);
+  assert(cr("1.2.3.4/29", {onlyHosts: true}).length === 6);
 });
 
 it("should return correct addresses", function () {
-    assert(cr("255.255.255.255/24")[0] === "255.255.255.0");
-    assert(cr("255.255.255.255/24")[255] === "255.255.255.255");
-    assert(cr("0.0.0.0/16")[0] === "0.0.0.0");
-    assert(cr("0.0.0.0/16")[255] === "0.0.0.255");
-    assert(cr("1.2.3.4/30", {onlyHosts: true})[0] === "1.2.3.5");
-
+  assert(cr("255.255.255.255/24")[0] === "255.255.255.0");
+  assert(cr("255.255.255.255/24")[255] === "255.255.255.255");
+  assert(cr("0.0.0.0/16")[0] === "0.0.0.0");
+  assert(cr("0.0.0.0/16")[255] === "0.0.0.255");
+  assert(cr("1.2.3.4/30", {onlyHosts: true})[0] === "1.2.3.5");
 });
